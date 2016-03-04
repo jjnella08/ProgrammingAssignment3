de97324b1527366342d62ababbc15917f47a7dd8
Jennifer <jjnella08@gmail.com>
2016-03-03 22:19:38 -0500

    added code to return the hospital name

--- best.R
+++ best.R
@@ -12,6 +12,33 @@ best <- function(state, outcome) {
     stop("Invalid Outcome - must be either heart attack, heart failure, or pneumonia")
   }
   
-  ## Return hospital name in that state with lowest 30-day death
+  ## Return hospital name in that state with lowest 30-day death?
   ## rate
+  
+  ## set the column index based in the outcome value
+  colIdx <- if (outcome == "heart attack") {
+    11
+  } else if (outcome == "heart failure") {
+    17
+  } else {
+    23
+  }
+  
+  ## create a subset of the data for the specified state
+  outcomeDF.sub1 <- outcomeDF[outcomeDF[,7] == state, ]
+  
+  ## create a subset of the State subset data for the specified outcome
+  outcomeDF.sub2 <- outcomeDF.sub1[,c(2,7,colIdx)]
+  
+  ## create a subset of the State/Outcome subset data of valid numbers
+  outcomeDF.sub3 <- outcomeDF.sub2[outcomeDF.sub2[,3]!="Not Available",]
+  
+  ## change the data to be numeric
+  outcomeDF.sub3[,3] <- as.numeric(outcomeDF.sub3[,3])
+  
+  ## find the row with the minimum value in the outcome column
+  minRow <- outcomeDF.sub3[outcomeDF.sub3[,3] == min(outcomeDF.sub3[, 3]),]
+  
+  ## get the Hospital name from the first column
+  return(as.character(minRow[1]))
 }
